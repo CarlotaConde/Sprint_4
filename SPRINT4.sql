7f9fffdbff1f520f1d9f90e45b3551691cab7cfd
Carlota Conde <carlota.conde.cc@gmail.com>
2024-10-02 13:46:41 +0200

    Update SPRINT4.sql

--- SPRINT4.sql
+++ SPRINT4.sql
@@ -114,3 +114,22 @@ CREATE TABLE IF NOT EXISTS users_uk (
 -- comprovació:
 SELECT *
 FROM users_uk;
+
+-- taula 'users_usa'
+CREATE TABLE IF NOT EXISTS users_usa (
+        id INT PRIMARY KEY,
+        name VARCHAR(20),
+        surname VARCHAR(20),
+        phone VARCHAR(15),
+        email VARCHAR(100),
+        birth_date VARCHAR(15),
+        country VARCHAR(100),
+        city VARCHAR(100),
+        postal_code VARCHAR(10),
+        address VARCHAR(200)
+    );
+    
+-- importar taula
+-- comprovació:
+SELECT *
+FROM users_usa;
