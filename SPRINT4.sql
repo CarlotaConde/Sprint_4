0c26a3ac584686258bd5d7fc3a0a0dc90b854fa9
Carlota Conde <carlota.conde.cc@gmail.com>
2024-10-14 12:54:59 +0200

    Update SPRINT4.sql

--- SPRINT4.sql
+++ SPRINT4.sql
@@ -8,7 +8,7 @@
 CREATE DATABASE IF NOT EXISTS bd;
 USE bd;
 
--- Taula companies:
+-- Crear la taula 'companies':
 CREATE TABLE IF NOT EXISTS companies (
         id VARCHAR(15) PRIMARY KEY,
         company_name VARCHAR(255),
@@ -18,17 +18,18 @@ CREATE TABLE IF NOT EXISTS companies (
         website VARCHAR(255)
     );
 
--- importar taula (arxiu companies)
+-- Un cop tenim tots els arxius '.csv' a la carpeta 'Uploads' de MySQL
+-- Importar l'arxiu 'companies.csv' a la taula creada 'companies'
 LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/companies.csv" 
 INTO TABLE companies
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
 
--- comprovació:
+-- Comprovació:
 SELECT *
 FROM companies;
 
--- Taula credit_cards:
+-- Crear la taula 'credit_cards':
 CREATE TABLE IF NOT EXISTS credit_cards (
     id VARCHAR(20) PRIMARY KEY,
     user_id VARCHAR(5),
@@ -41,17 +42,17 @@ CREATE TABLE IF NOT EXISTS credit_cards (
     expiring_date VARCHAR(10)
 );
 
--- importar taula (arxiu credit_cards)
+-- Importar l'arxiu 'credit_cards.csv' a la taula creada 'credit_cards'
 LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/credit_cards.csv" 
 INTO TABLE credit_cards
 FIELDS TERMINATED BY ',' 
 IGNORE 1 LINES;
 
--- comprovació:
+-- Comprovació:
 SELECT *
 FROM credit_cards;
 
--- Taula products:
+-- Crear la taula 'products':
 CREATE TABLE IF NOT EXISTS products (
     id VARCHAR(20) PRIMARY KEY,
     product_name VARCHAR(50),
@@ -61,17 +62,17 @@ CREATE TABLE IF NOT EXISTS products (
     warehouse_id VARCHAR(10)
 );
 
--- importar taula (arxiu products)
+-- Importar l'arxiu 'products.csv' a la taula creada 'products'
 LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/products.csv" 
 INTO TABLE products
 FIELDS TERMINATED BY ',' 
 IGNORE 1 LINES;
 
--- comprovació:
+-- Comprovació:
 SELECT *
 FROM products;
 
--- Taula transactions:
+-- Crear la taula 'transactions':
 CREATE TABLE IF NOT EXISTS transactions (
         id VARCHAR(255) PRIMARY KEY,
         card_id VARCHAR(10),
@@ -85,17 +86,17 @@ CREATE TABLE IF NOT EXISTS transactions (
         longitude FLOAT
     );
     
--- importar taula (arxiu transactions)
+-- Importar l'arxiu 'transactions.csv' a la taula creada 'transactions'
 LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/transactions.csv" 
 INTO TABLE transactions
 FIELDS TERMINATED BY ';' 
 IGNORE 1 LINES;
 
--- comprovació:
+-- Comprovació:
 SELECT *
 FROM transactions;
 
--- Taula users
+-- Crear la taula 'users':
 CREATE TABLE IF NOT EXISTS users (
         id INT PRIMARY KEY,
         name VARCHAR(20),
@@ -109,56 +110,61 @@ CREATE TABLE IF NOT EXISTS users (
         address VARCHAR(200)
     );
     
--- importar taules (3 arxius: users_ca, users_uk i users_usa)
+-- Importar les taules (3 arxius: users_ca.csv, users_uk.csv i users_usa.csv)
 -- Taula 'USA'
+-- Importar l'arxiu 'users_usa.csv' a la taula creada 'users'
 LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/users_usa.csv" 
 INTO TABLE users
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 -- Taula 'UK'
+-- Importar l'arxiu 'users_uk.csv' a la taula creada 'users'
 LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/users_uk.csv" 
 INTO TABLE users
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 -- Taula 'CA'
+-- Importar l'arxiu 'users_ca.csv' a la taula creada 'users'
 LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/users_ca.csv" 
 INTO TABLE users
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
 
--- comprovació:
+-- Comprovació:
 SELECT *
 FROM users;
 
--- creem INDEXS i FK:
+-- Creem els 'Índexs' i les 'Foreign Keys'(FK) per poder estbalir les relacions entre les taules:
+-- Primer creo l'índex a la taula 'transactions', ja que aquesta taula és el punt d'unió de totes (model estrella).
+-- Després indico quina es la FK per després indicar-li a quina columna de la taula fa referència.
 
--- COMPANIES:
+-- Taula 'companies':
 ALTER TABLE transactions
 ADD INDEX idx_business_id (business_id ASC);
 ALTER TABLE transactions
 ADD FOREIGN KEY (business_id) REFERENCES companies(id);
 
--- CREDIT_CARDS:
+-- Taula 'credit_cards':
 ALTER TABLE transactions
 ADD INDEX idx_card_id (card_id ASC);
 ALTER TABLE transactions
 ADD FOREIGN KEY (card_id) REFERENCES credit_cards(id);
 
--- PRODUCTS:
+-- Taula 'products':
 ALTER TABLE transactions
 ADD INDEX idx_products_ids (products_ids ASC);
--- dona error al establir la relació, ho soluciono fent:
+-- Surt un error al establir la relació, ho soluciono fent:
 SET FOREIGN_KEY_CHECKS=0;
--- estableixo la relació:
+-- Estableixo la relació:
 ALTER TABLE transactions
 ADD FOREIGN KEY (products_ids) REFERENCES products(id);
--- ho tornem a activar
+-- Ho tornem a activar:
 SET FOREIGN_KEY_CHECKS=1;
 
--- USERS:
+-- Taula 'users':
 ALTER TABLE transactions
 ADD INDEX idx_user_id (user_id ASC);
 ALTER TABLE transactions
@@ -167,8 +173,11 @@ ADD FOREIGN KEY (user_id) REFERENCES users(id);
 
 -- EXERCICI 1:
 -- Realitza una subconsulta que mostri tots els usuaris amb més de 30 transaccions utilitzant almenys 2 taules.
+-- L'he fet amb subquery i amb JOIN, fent-lo amb JOIN per poder visualitzar més dades que fent una subquery no surten.
+-- A més, després ho he fet de 2 maneres, en una he mostrat tota la informació de la consulta, ja sigui que les transaccions que han fet els usuaris s'hagin denegat o no,
+-- i en l'altre filtrant solament per aquelles transaccions que no han sigut denegades i si que s'han dut a terme.
 
--- amb subquery (totes les transaccions, denegades o no):
+-- Amb subquery (totes les transaccions, denegades o no):
 SELECT  
   id AS 'ID',
   name AS 'Nom', 
@@ -183,7 +192,7 @@ WHERE users.id IN (SELECT user_id
 		   GROUP BY user_id
 		   HAVING COUNT(transactions.id) >30);
                     
--- amb subquery (solament les no denegades):
+-- Amb subquery (solament les no denegades):
 SELECT  
   id AS 'ID',
   name AS 'Nom', 
@@ -200,7 +209,7 @@ WHERE users.id IN (SELECT user_id
 		   HAVING COUNT(transactions.id) >30);
 
 
--- amb JOIN (totes les transaccions, denegades o no):
+-- Amb JOIN (totes les transaccions, denegades o no):
 SELECT  
   COUNT(transactions.id) AS 'Total de transaccions', 
   SUM(transactions.amount) AS 'Quantitat total (€)',
@@ -219,7 +228,7 @@ GROUP BY
 HAVING COUNT(transactions.id) >30
 ORDER BY COUNT(transactions.id) DESC;
 
--- amb JOIN (solament les no denegades)
+-- Amb JOIN (solament les no denegades)
 SELECT  
   COUNT(transactions.id) AS 'Total de transaccions', 
   SUM(transactions.amount) AS 'Quantitat total (€)',
@@ -242,7 +251,7 @@ ORDER BY COUNT(transactions.id) DESC;
 -- EXERCICI 2:
 -- Mostra la mitjana d'amount per IBAN de les targetes de crèdit a la companyia Donec Ltd, utilitza almenys 2 taules.
 
--- amb subquery:
+-- Amb subquery (per veure de manera resumida la mitjana):
 SELECT 
   ROUND(AVG(amount),2) AS 'Mitja (€)'
 FROM transactions
@@ -250,7 +259,7 @@ WHERE transactions.business_id IN (SELECT companies.id
 				   FROM companies
                                    WHERE company_name = 'Donec Ltd');
 
--- amb JOIN:
+-- Amb JOIN (per veure a part de la mitjana, més informació adicional):
 SELECT 
   ROUND(AVG(transactions.amount),2) AS 'Mitja (€)', 
   credit_cards.iban AS 'IBAN', 
@@ -281,11 +290,11 @@ JOIN credit_cards
 ON transactions.card_id = credit_cards.id
 GROUP BY credit_cards.id, credit_cards.iban;
 
--- comprovar
+-- Comprovació:
 SELECT *
 FROM card_estat;
 
--- crear relació amb la taula 'credit_cards'
+-- Crear la relació amb la taula 'credit_cards', crent primer l'índex i després relacionant la FK amb la columna de l'altre taula:
 ALTER TABLE credit_cards
 ADD INDEX idx_card_id (id ASC);
 ALTER TABLE card_estat
@@ -298,6 +307,7 @@ SELECT
   COUNT(id) AS 'Targetes actives'
 FROM card_estat
 WHERE Estat = 'activa';
+-- El resultat vol dir que totes estan actives, ja que el número total de targetes es 275.
 
 -- NIVELL 3
 -- Crea una taula amb la qual puguem unir les dades del nou arxiu products.csv amb la base de dades creada, tenint en compte que des de transaction tens product_ids. Genera la següent consulta:
@@ -306,6 +316,7 @@ WHERE Estat = 'activa';
 
 -- EXERCICI 1
 -- Necessitem conèixer el nombre de vegades que s'ha venut cada producte.
+
 SELECT 
   COUNT(transactions.id) AS 'Quantitat de vegades venut',
   products.product_name AS 'Nom del producte',
