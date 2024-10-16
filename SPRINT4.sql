07d31cc1601c77a74d1ec12d003b101075e73fd4
Carlota Conde <carlota.conde.cc@gmail.com>
2024-10-16 17:52:21 +0200

    Update SPRINT4.sql

--- SPRINT4.sql
+++ SPRINT4.sql
@@ -52,26 +52,6 @@ IGNORE 1 LINES;
 SELECT *
 FROM credit_cards;
 
--- Crear la taula 'products':
-CREATE TABLE IF NOT EXISTS products (
-    id VARCHAR(20) PRIMARY KEY,
-    product_name VARCHAR(50),
-    price VARCHAR(10),
-    colour VARCHAR(7),
-    weight VARCHAR(4),
-    warehouse_id VARCHAR(10)
-);
-
--- Importar l'arxiu 'products.csv' a la taula creada 'products'
-LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/products.csv" 
-INTO TABLE products
-FIELDS TERMINATED BY ',' 
-IGNORE 1 LINES;
-
--- Comprovació:
-SELECT *
-FROM products;
-
 -- Crear la taula 'transactions':
 CREATE TABLE IF NOT EXISTS transactions (
         id VARCHAR(255) PRIMARY KEY,
@@ -153,17 +133,6 @@ ADD INDEX idx_card_id (card_id ASC);
 ALTER TABLE transactions
 ADD FOREIGN KEY (card_id) REFERENCES credit_cards(id);
 
--- Taula 'products':
-ALTER TABLE transactions
-ADD INDEX idx_products_ids (products_ids ASC);
--- Surt un error al establir la relació, ho soluciono fent:
-SET FOREIGN_KEY_CHECKS=0;
--- Estableixo la relació:
-ALTER TABLE transactions
-ADD FOREIGN KEY (products_ids) REFERENCES products(id);
--- Ho tornem a activar:
-SET FOREIGN_KEY_CHECKS=1;
-
 -- Taula 'users':
 ALTER TABLE transactions
 ADD INDEX idx_user_id (user_id ASC);
@@ -173,11 +142,7 @@ ADD FOREIGN KEY (user_id) REFERENCES users(id);
 
 -- EXERCICI 1:
 -- Realitza una subconsulta que mostri tots els usuaris amb més de 30 transaccions utilitzant almenys 2 taules.
--- L'he fet amb subquery i amb JOIN, fent-lo amb JOIN per poder visualitzar més dades que fent una subquery no surten.
--- A més, després ho he fet de 2 maneres, en una he mostrat tota la informació de la consulta, ja sigui que les transaccions que han fet els usuaris s'hagin denegat o no,
--- i en l'altre filtrant solament per aquelles transaccions que no han sigut denegades i si que s'han dut a terme.
 
--- Amb subquery (totes les transaccions, denegades o no):
 SELECT  
   id AS 'ID',
   name AS 'Nom', 
@@ -192,91 +157,21 @@ WHERE users.id IN (SELECT user_id
 		   GROUP BY user_id
 		   HAVING COUNT(transactions.id) >30);
                     
--- Amb subquery (solament les no denegades):
-SELECT  
-  id AS 'ID',
-  name AS 'Nom', 
-  surname AS 'Cognom', 
-  country AS 'Pais', 
-  city AS 'Ciutat', 
-  postal_code AS 'Codi postal',
-  phone AS 'Telèfon'
-FROM users
-WHERE users.id IN (SELECT user_id
-		   FROM transactions
-		   WHERE declined = 0
-		   GROUP BY user_id
-		   HAVING COUNT(transactions.id) >30);
-
-
--- Amb JOIN (totes les transaccions, denegades o no):
-SELECT  
-  COUNT(transactions.id) AS 'Total de transaccions', 
-  SUM(transactions.amount) AS 'Quantitat total (€)',
-  user_id AS "ID de l'usuari", 
-  users.name AS 'Nom', 
-  users.surname AS 'Cognom', 
-  users.country AS 'Pais', 
-  users.city AS 'Ciutat', 
-  users.postal_code AS 'Codi postal',
-  users.phone AS 'Telèfon'
-FROM transactions
-JOIN users
-ON transactions.user_id = users.id
-GROUP BY 
-  user_id, users.name, users.surname, users.country, users.city, users.postal_code, users.phone
-HAVING COUNT(transactions.id) >30
-ORDER BY COUNT(transactions.id) DESC;
-
--- Amb JOIN (solament les no denegades)
-SELECT  
-  COUNT(transactions.id) AS 'Total de transaccions', 
-  SUM(transactions.amount) AS 'Quantitat total (€)',
-  user_id AS "ID de l'usuari", 
-  users.name AS 'Nom', 
-  users.surname AS 'Cognom', 
-  users.country AS 'Pais', 
-  users.city AS 'Ciutat', 
-  users.postal_code AS 'Codi postal',
-  users.phone AS 'Telèfon'
-FROM transactions
-JOIN users
-ON transactions.user_id = users.id
-WHERE transactions.declined = 0
-GROUP BY 
-  user_id, users.name, users.surname, users.country, users.city, users.postal_code, users.phone
-HAVING COUNT(transactions.id) >30
-ORDER BY COUNT(transactions.id) DESC;
-
 -- EXERCICI 2:
 -- Mostra la mitjana d'amount per IBAN de les targetes de crèdit a la companyia Donec Ltd, utilitza almenys 2 taules.
-
--- Amb subquery (per veure de manera resumida la mitjana):
 SELECT 
-  ROUND(AVG(amount),2) AS 'Mitja (€)'
-FROM transactions
-WHERE transactions.business_id IN (SELECT companies.id
-				   FROM companies
-                                   WHERE company_name = 'Donec Ltd');
-
--- Amb JOIN (per veure a part de la mitjana, més informació adicional):
-SELECT 
-  ROUND(AVG(transactions.amount),2) AS 'Mitja (€)', 
   credit_cards.iban AS 'IBAN', 
-  companies.company_name AS 'Nom de la companyia', 
-  companies.country AS 'Pais'
-FROM transactions
-JOIN credit_cards
+  ROUND(AVG(transactions.amount),2) AS 'Mitja (€)'
+FROM transactions 
+JOIN credit_cards  
 ON transactions.card_id = credit_cards.id
-JOIN companies
+JOIN companies  
 ON transactions.business_id = companies.id
 WHERE companies.company_name = 'Donec Ltd'
-GROUP BY credit_cards.iban, companies.company_name, companies.country
-ORDER BY  ROUND(AVG(transactions.amount),2) DESC;
+GROUP BY credit_cards.iban;
 
 -- NIVELL 2
 -- Crea una nova taula que reflecteixi l'estat de les targetes de crèdit basat en si les últimes tres transaccions van ser declinades i genera la següent consulta:
-
 CREATE TABLE card_estat AS
 SELECT 
     credit_cards.id,
@@ -295,14 +190,11 @@ SELECT *
 FROM card_estat;
 
 -- Crear la relació amb la taula 'credit_cards', crent primer l'índex i després relacionant la FK amb la columna de l'altre taula:
-ALTER TABLE credit_cards
-ADD INDEX idx_card_id (id ASC);
 ALTER TABLE card_estat
 ADD FOREIGN KEY (id) REFERENCES credit_cards(id);
 
 -- EXERCICI 1
 -- Quantes targetes estan actives?
-
 SELECT 
   COUNT(id) AS 'Targetes actives'
 FROM card_estat
@@ -312,11 +204,46 @@ WHERE Estat = 'activa';
 -- NIVELL 3
 -- Crea una taula amb la qual puguem unir les dades del nou arxiu products.csv amb la base de dades creada, tenint en compte que des de transaction tens product_ids. Genera la següent consulta:
 
--- (taula creada al principi de l'exercici)
+-- Crear la taula 'products':
+CREATE TABLE IF NOT EXISTS products (
+    id VARCHAR(20) PRIMARY KEY,
+    product_name VARCHAR(50),
+    price VARCHAR(10),
+    colour VARCHAR(7),
+    weight VARCHAR(4),
+    warehouse_id VARCHAR(10)
+);
+
+-- Importar l'arxiu 'products.csv' a la taula creada 'products'
+LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/products.csv" 
+INTO TABLE products
+FIELDS TERMINATED BY ',' 
+IGNORE 1 LINES;
+
+-- Comprovació:
+SELECT *
+FROM products;
+
+
+-- He intentat crear una taula intermitja de ‘transaction_product’, 
+-- per fer l’unió entre ‘products’ i ‘transactions’ per no em sortía com separar els ‘id’, 
+-- perque a la taula ‘transactions’ els ‘products_ids’ están separats per comes.
+
+-- Creem l'índex i la 'Foreign Key'(FK) per poder estbalir la relació entre les taules 'products' i 'transactions'
+-- Primer creo l'índex a la taula 'transactions', ja que aquesta taula és el punt d'unió de totes (model estrella).
+-- Després indico quina es la FK per després indicar-li a quina columna de la taula fa referència.
+ALTER TABLE transactions
+ADD INDEX idx_products_ids (products_ids ASC);
+-- Surt un error al establir la relació, ho soluciono fent (sé que no es el més convenient, però es de la única manera que ho he pogut solucionar):
+SET FOREIGN_KEY_CHECKS=0;
+-- Estableixo la relació:
+ALTER TABLE transactions
+ADD FOREIGN KEY (products_ids) REFERENCES products(id);
+-- Ho tornem a activar:
+SET FOREIGN_KEY_CHECKS=1;
 
 -- EXERCICI 1
 -- Necessitem conèixer el nombre de vegades que s'ha venut cada producte.
-
 SELECT 
   COUNT(transactions.id) AS 'Quantitat de vegades venut',
   products.product_name AS 'Nom del producte',
